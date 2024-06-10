dfdb4cf75e89e1bc07365fb775b0ba426aba4cb9
Renyxx <dev.shubhangi1510@gmail.com>
2024-06-10 21:10:40 +0530

    Initial commit

---
+++ src/main/java/org/elabs/eventregistration/EventRegistrationApplication.java
@@ -0,0 +1,15 @@
+package org.elabs.eventregistration;
+
+//admin admin@1234
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventRegistrationApplication.class, args);
+    }
+
+}
